59c263cd5c4bcccf5dafd2ef1fca974695040422
a1usha <aleksandr.ushaev@hotmail.com>
2021-03-07 13:16:57 +0700

    modify ojp 3 lab

--- ojp_3/src/main/java/ru/nsu/fit/ojp/ushaev/ojp_3/MyJavaAgent.java
+++ ojp_3/src/main/java/ru/nsu/fit/ojp/ushaev/ojp_3/MyJavaAgent.java
@@ -33,42 +33,21 @@ public class MyJavaAgent {
                     ClassPool cp = ClassPool.getDefault();
                     CtClass cc = cp.makeClass(new ByteArrayInputStream(classfileBuffer));
 
-                    CtField min = CtField.make("private static float min;", cc);
-                    CtField total = CtField.make("private static float total;", cc);
-                    CtField max = CtField.make("private static float max;", cc);
-                    CtField cnt = CtField.make("private static int cnt;", cc);
-                    CtField tmpTimeMillis = CtField.make("private static long tmpTimeMillis;", cc);
-                    CtField tmpTimeSec = CtField.make("private static float tmpTimeSec;", cc);
-
-                    cc.addField(min, "1.1F");
-                    cc.addField(total, "0F");
-                    cc.addField(max, "-1F");
-                    cc.addField(cnt, "0");
-                    cc.addField(tmpTimeMillis, "0L");
-                    cc.addField(tmpTimeSec, "0F");
-
                     // Modify processTransaction metod - add 99 to first argument
                     CtMethod mProcessTransaction = cc.getDeclaredMethod("processTransaction");
                     mProcessTransaction.insertBefore("{$1 += 99;}");
-                    // Rename method to use it in proxy method with same name
-                    mProcessTransaction.setName("tmp");
+                    mProcessTransaction.addLocalVariable("startTime", CtClass.longType);
+                    String methodName = mProcessTransaction.getLongName();
+                    mProcessTransaction.insertBefore("{startTime = System.currentTimeMillis();}");
+                    mProcessTransaction.insertAfter(
+                            "{ru.nsu.fit.ojp.ushaev.ojp_3.TimeRegistry.instance.addMetric(\"" + methodName + "\", System.currentTimeMillis() - startTime);}");
 
-                    // Create new proxy method to measure min, avg and max execution time of processTransaction method
-                    CtMethod proxyProcessTransaction = CtNewMethod.make(
-                            "public void processTransaction(int txNum) throws Exception {" +
-                                    "tmpTimeMillis = System.currentTimeMillis();" +
-                                    "tmp(txNum);" +
-                                    "tmpTimeSec = (System.currentTimeMillis() - tmpTimeMillis) / 1000F;" +
-                                    "min = tmpTimeSec < min ? tmpTimeSec : min;" +
-                                    "total += tmpTimeSec;" +
-                                    "cnt += 1;" +
-                                    "max = tmpTimeSec > max ? tmpTimeSec : max;" +
-                                    "}", cc);
-                    cc.addMethod(proxyProcessTransaction);
 
                     // Visit method main
                     CtMethod main = cc.getDeclaredMethod("main");
-                    main.insertAfter("{System.out.println(\"Min: \" + min + \", Avg: \" + total/cnt + \", Max: \" + max);}");
+                    main.insertAfter("{System.out.println(ru.nsu.fit.ojp.ushaev.ojp_3.TimeRegistry.instance.getMinTime(\"" + methodName + "\"));}");
+                    main.insertAfter("{System.out.println(ru.nsu.fit.ojp.ushaev.ojp_3.TimeRegistry.instance.getAvgTime(\"" + methodName + "\"));}");
+                    main.insertAfter("{System.out.println(ru.nsu.fit.ojp.ushaev.ojp_3.TimeRegistry.instance.getMaxTime(\"" + methodName + "\"));}");
 
                     // Get bytecode
                     byte[] modifiedClassfileBuffer = cc.toBytecode();
